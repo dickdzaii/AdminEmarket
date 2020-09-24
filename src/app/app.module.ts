bf2711e23bf5b983ae361cb36c3e24a6f2ba49f4
dickdzaii <70245065+dickdzaii@users.noreply.github.com>
2020-09-24 16:55:14 +0700

    add basic files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DanhsachComponent } from './danhsach/danhsach.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { PageloiComponent } from './layouts/pageloi/pageloi.component';
+import { BaseComponent } from './services/base/base.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DanhsachComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageloiComponent,
+    BaseComponent,
   ],
   imports: [
     BrowserModule,
