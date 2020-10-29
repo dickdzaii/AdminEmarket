7801d8bb607aa1c848d1ef8671d92e63b61ca696
dickdzaii <70245065+dickdzaii@users.noreply.github.com>
2020-10-29 15:49:17 +0700

    update templates w data

--- src/app/main/donhang/donhang.component.ts
+++ src/app/main/donhang/donhang.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/services/base.component';
 
 @Component({
   selector: 'app-donhang',
   templateUrl: './donhang.component.html',
   styleUrls: ['./donhang.component.css']
 })
-export class DonhangComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class DonhangComponent extends BaseComponent implements OnInit {
+index:any;
+size:any;
+val:any;
+donhangs:any;
+total:any;
+  constructor(injector:Injector) {
+    super(injector);
   }
 
+  ngOnInit(): void {
+    this.index=1;this.size=20;
+    this.donhangs=[];
+    this.total=0;
+      this._api.get('api/QLDonHang/getbyshop/s0001/'+this.index+'/'+this.size).takeUntil(this.unsubscribe).subscribe(res => {
+      this.val = res;
+      this.donhangs=this.val.data;
+      this.total=this.val.totalItems;
+    
+    }, err => { });
+  }
+  loadPage(page){
+    this._api.get('api/QLDonHang/getbyshop/s0001/'+page+'/'+this.size).takeUntil(this.unsubscribe).subscribe(res => {
+      this.val = res;
+      this.donhangs=this.val.data;
+      this.total=this.val.totalItems;
+
+    }, err => { });
+  }
+  changeStatus(){
+    console.log(0);
+  }
 }
---
+++ src/app/main/khachhang/khachhang.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KhachhangComponent } from './khachhang.component';
+
+
+
+@NgModule({
+  declarations: [KhachhangComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class KhachhangModule { }
